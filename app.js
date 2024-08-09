27760a58a64e4262c95406dc536c8baf46e270b2
Edwin Moreno <edwinmoreno77@gmail.com>
2024-08-09 17:51:57 +0000

    exercise unit test finished

--- app.js
+++ app.js
@@ -7,19 +7,20 @@ let oneEuroIs = {
 
 const fromEuroToDollar = function(valueInEuro) {
     let valueInDollar = valueInEuro * oneEuroIs.USD;
-    return parseFloat(valueInDollar.toFixed(2));
+    return valueInDollar;
 };
-
 const fromDollarToYen = function(valueInDollar) {
+    // Converts US Dollars to Japanese Yen (using an intermediate conversion to Euros)
     let valueInEuro = valueInDollar / oneEuroIs.USD;
     let valueInYen = valueInEuro * oneEuroIs.JPY;
-    return parseFloat(valueInYen.toFixed(2));
+    return valueInYen;
 };
 
 const fromYenToPound = function(valueInYen) {
+    // Converts Japanese Yen to British Pounds (using an intermediate conversion to Euros)
     let valueInEuro = valueInYen / oneEuroIs.JPY;
     let valueInPound = valueInEuro * oneEuroIs.GBP;
-    return parseFloat(valueInPound.toFixed(2));
+    return valueInPound;
 };
 
 module.exports = {
--- test.js
+++ test.js
@@ -4,30 +4,27 @@ test("One euro should be 1.07 dollars", function() {
     const dollars = fromEuroToDollar(3.5);
     
     let valueInDollar = 3.5 * oneEuroIs.USD;
-    let roundedValueInDollar = valueInDollar.toFixed(2);
-    const expected = parseFloat(roundedValueInDollar);
+    const expected =valueInDollar;
     
-    expect(dollars).toBe(expected);
+    expect(dollars).toBeCloseTo(expected);
 });
 
 test("One dollar should be 146.26 yen", function() {
     const yen = fromDollarToYen(3.5);
-    
+    // Converts US Dollars to Japanese Yen (using an intermediate conversion to Euros)
     let valueInEuro = 3.5 / oneEuroIs.USD;
     let valueInYen = valueInEuro * oneEuroIs.JPY;
-    let roundedValueInYen = valueInYen.toFixed(2);
-    const expected = parseFloat(roundedValueInYen);
+    const expected = valueInYen
     
-    expect(yen).toBe(expected);
+    expect(yen).toBeCloseTo(expected);
 });
 
 test("One yen should be 0.005 pounds", function() {
     const pound = fromYenToPound(3.5);
-    
+    // Converts Japanese Yen to British Pounds (using an intermediate conversion to Euros)
     let valueInEuro = 3.5 / oneEuroIs.JPY;
     let valueInPound = valueInEuro * oneEuroIs.GBP;
-    let roundedValueInPound = valueInPound.toFixed(2);
-    const expected = parseFloat(roundedValueInPound);
+    const expected = valueInPound;
     
-    expect(pound).toBe(expected);
+    expect(pound).toBeCloseTo(expected);
 });
